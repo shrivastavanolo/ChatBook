a1a550d36e2982edd5bcdaea189fe3fd0534c327
Shreya Shrivastava <126598622+shrivastavanolo@users.noreply.github.com>
2023-09-07 02:49:32 +0530

    Update chatty.py

--- chatty.py
+++ chatty.py
@@ -22,7 +22,7 @@ from pymongo.mongo_client import MongoClient
 uri = "mongodb+srv://alinakapoor2002:Jd4jOtIbOonIHWgC@cluster0.kqz0gln.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri,ssl=True,
-    ssl_cert_reqs=ssl.CERT_NONE,)
+    ssl_cert_reqs=CERT_NONE,)
 db=client.Images
 table=db.Imagebase
 
